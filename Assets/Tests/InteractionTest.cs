c5421530534de454909600cee1811e81ab764ff9
nigoroku <nigoroku0120@gmail.com>
2021-03-04 23:18:39 +0900

    first commit

---
+++ Assets/Tests/InteractionTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class InteractionTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            SceneManager.UnloadSceneAsync("MainScene");
+        }
+
+        private class MonoBehaviour_TestScenario : MonoBehaviour, IMonoBehaviourTest
+        {
+            private Renderer cubeRenderer;
+
+            public bool IsTestFinished { get; private set; }
+
+            private void Start()
+            {
+                cubeRenderer = GameObject.Find("Cube").GetComponent<Renderer>();
+            }
+
+            private void Update()
+            {
+                if (cubeRenderer.material.color == Color.red)
+                    IsTestFinished = true;
+            }
+        }
+
+        [UnityTest]
+        public IEnumerator ボタンを押すとCubeが赤くなる()
+        {
+            yield return new MonoBehaviourTest<MonoBehaviour_TestScenario>();
+        }
+    }
+}
---
+++ Assets/Translation.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Translation;
+using Microsoft.MixedReality.Toolkit.UI;
+using TMPro;
+using System;
+
+public class Translation : MonoBehaviour
+{
+        
+    public TextMeshPro recognizedText;
+    public TextMeshPro translatedText;
+    public PressableButton micButton;
+
+    public string SpeechServiceSubscriptionKey = "";
+    public string SpeechServiceRegion = "";
+
+    private bool waitingforReco;
+    private string recognizedString;
+    private string translatedString;
+
+    private bool micPermissionGranted = false;
+
+    private object threadLocker = new object();
+
+    public async void ButtonClick()
+    {
+        var translationConfig = SpeechTranslationConfig.FromSubscription(SpeechServiceSubscriptionKey, SpeechServiceRegion);
+        translationConfig.SpeechRecognitionLanguage = "en-US";
+        translationConfig.AddTargetLanguage("fr");
+
+        using (var recognizer = new TranslationRecognizer(translationConfig))
+        {
+            lock (threadLocker)
+            {
+                waitingforReco = true;
+            }
+
+            var result = await recognizer.RecognizeOnceAsync().ConfigureAwait(false);
+
+            if (result.Reason == ResultReason.TranslatedSpeech)
+            {
+                recognizedString = result.Text;
+                foreach (var element in result.Translations)
+                {
+                    translatedString = element.Value;
+                }
+            }
+            else if (result.Reason == ResultReason.NoMatch)
+            {
+                recognizedString = "NOMATCH: Speech could not be recognized.";
+            }
+            else if (result.Reason == ResultReason.Canceled)
+            {
+                var cancellation = CancellationDetails.FromResult(result);
+                recognizedString = $"CANCELED: Reason={cancellation.Reason} ErrorDetails={cancellation.ErrorDetails}";
+            }
+
+            lock (threadLocker)
+            {
+                waitingforReco = false;
+            }
+
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (translatedText == null)
+        {
+            UnityEngine.Debug.LogError("translatedText property is null! Assign a UI TextMeshPro Text element to it.");
+        }
+        else if (micButton == null)
+        {
+            UnityEngine.Debug.LogError("micButton property is null! Assign a MRTK Pressable Button to it.");
+        }
+        else
+        {
+            micPermissionGranted = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lock (threadLocker)
+        {
+            recognizedText.text = recognizedString;
+            translatedText.text = translatedString;
+        }
+    }
+}
